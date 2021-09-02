972e944cfbb48ca26e7047512e7dab919d6f0d95
adam.harrison <adam.harrison@engineering.digital.dwp.gov.uk>
2021-09-02 21:46:44 +0100

    test added to verify controller calls UserService

---
+++ src/main/java/com/example/findusersservice/services/UserService.java
@@ -0,0 +1,2 @@
+package com.example.findusersservice.services;public interface UserService {
+}
--- src/test/java/com/example/findusersservice/controllers/FindUsersControllerImplTest.java
+++ src/test/java/com/example/findusersservice/controllers/FindUsersControllerImplTest.java
@@ -3,8 +3,11 @@ package com.example.findusersservice.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,10 +17,22 @@ class FindUsersControllerImplTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    protected UserService mockUserService;
+
     @Test
     void controller_returns_200_response_on_GET() throws Exception {
 
         mockMvc.perform(get("/v1/london")).andExpect(status().isOk());
+
+    }
+
+    @Test
+    void controller_calls_user_service() throws Exception {
+
+        mockMvc.perform(get("/v1/london")).andExpect(status().isOk());
+
+        verify(mockUserService, times(1)).getUsers();
 
     }
 
